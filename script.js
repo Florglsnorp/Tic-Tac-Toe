3c13dd2c27ee15d536a4e2c41943ba5d6c142571
Florglsnorp <nicholaslink234@gmail.com>
2023-11-09 10:01:53 -0500

    Changed some

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let gameEnded = false
 let xScore = 0
 let oScore = 0
 
-
-for (let i = 1; i <= 9; i++){
+for (let i = 1; i <= 9; i++)
+{
     document.getElementById(i.toString()).addEventListener(
         "click",
-        function(){
-        if (this.innerHTML === "" && !gameEnded){
+        function()
+        {
+        if (this.innerHTML === "" && !gameEnded)
+        {
             this.innerHTML = playerSymbol
             this.classList.add(playerSymbol.toLocaleLowerCase())
             checkWin()
@@ -25,7 +27,8 @@ for (let i = 1; i <= 9; i++){
 }
 
 
-let winPos = [
+let winPos = 
+[
     [1, 2, 3], [4, 5, 6],
     [7, 8, 9], [1, 4, 7],
     [2, 5, 8], [3, 6, 9],
@@ -33,41 +36,52 @@ let winPos = [
 ]
 
 
-function checkWin() {
-    for (let i = 0; i < winPos.length; i++) {
-      if (
+function checkWin()
+{
+    for (let i = 0; i < winPos.length; i++)
+    {
+      if
+      (
         document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
         document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
         document.getElementById(winPos[i][2]).innerHTML === playerSymbol
-        ){
+        )
+        {
         document.getElementById(winPos[i][0]).classList.add("win");
         document.getElementById(winPos[i][1]).classList.add("win");
         document.getElementById(winPos[i][2]).classList.add("win"); 
         gameEnded = true;
 
-        setTimeout(function() {
-            if(playerSymbol === "X"){
-                alert("O wins!");}
+        setTimeout(function()
+        {
+            if(playerSymbol === "X")
+            {
+                alert("O wins!");
                 oScore += 1
-            if(playerSymbol === "O"){
-                alert("X wins!")}
+                document.getElementById("p2").innerHTML = oScore
+            }
+            if(playerSymbol === "O")
+            {
+                alert("X wins!")
                 xScore += 1
+                document.getElementById("p1").innerHTML = xScore
+            }
         }, 500);
-    }
+        }
     }
 }
 
 
 document.getElementById("reset").addEventListener(
     "click",
-    function(){
+    function()
+    {
         for (let i = 1; i <= 9; i++){
             document.getElementById(i.toString()).innerHTML = ""
             document.getElementById(i.toString()).classList.remove("x")
             document.getElementById(i.toString()).classList.remove("o")
             document.getElementById(i.toString()).classList.remove("win")
             gameEnded = false;
-
         }
     }
 )
